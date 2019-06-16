2e7bfbb6c57d8c2508b863bb4ef00ff6d00251dc
Memitaru <amandatwah@gmail.com>
2019-06-16 14:49:17 -0700

    MVP

--- api/server.js
+++ api/server.js
@@ -2,8 +2,31 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
+const session = require('express-session');
+const SessionStore = require('connect-session-knex')(session);
 
 const server = express();
+
+const sessionConfig = {
+    name: 'session',
+    secret: 'session secret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 60 * 60 * 1000,
+        secure: false,
+        httpOnly: false
+    },
+    store: new SessionStore({
+        knex: require('../database/dbConfig'),
+        tablename: 'sessions',
+        sidfieldname: 'sid',
+        createtable: true,
+        clearInterval: 60 * 60 * 1000
+    })
+}
+
+server.use(session(sessionConfig))
 server.use(express.json());
 server.use(cors());
 server.use(helmet());
@@ -44,6 +67,7 @@ server.post('/api/login', (req, res) => {
     .first()
     .then(user => {
         if (user && bcrypt.compareSync(password, user.password)){
+            req.session.user = user;
             res.status(200).json({message: `Welcome ${username}`})
         } else {
             res.status(401).json({message: "Invalid credentials"})
@@ -69,25 +93,30 @@ server.get('/api/restricted/users', authorize, (req, res) => {
 // Authorization Middleware
 
 function authorize(req, res, next){
-    const username = req.headers['x-username'];
-    const password = req.headers['x-password'];
+    // const username = req.headers['x-username'];
+    // const password = req.headers['x-password'];
 
-    if (!username||!password){
-        res.status(401).json({message: "Invalid credentials"})
+    // if (!username||!password){
+    //     res.status(401).json({message: "Invalid credentials"})
+    // }
+
+    // Users.findBy({username})
+    // .first()
+    // .then(user => {
+    //     if(user && bcrypt.compareSync(password, user.password)){
+    //         next()
+    //     } else {
+    //         res.status(401).json({message: "Invalid credentials"})
+    //     }
+    // })
+    // .catch(err => {
+    //     res.status(500).json(err)
+    // })
+    if (req.session && req.session.user){
+        next()
+    } else {
+        res.status(401).json({message: "You are not authorized"})
     }
-
-    Users.findBy({username})
-    .first()
-    .then(user => {
-        if(user && bcrypt.compareSync(password, user.password)){
-            next()
-        } else {
-            res.status(401).json({message: "Invalid credentials"})
-        }
-    })
-    .catch(err => {
-        res.status(500).json(err)
-    })
 }
 
 module.exports = server;
